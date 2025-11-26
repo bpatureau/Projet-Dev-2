d66ac80bc21a69bf2f9c5f1cc645cbc7c2f2a22e
Caribouspatial <ju.aerts@students.ephec.be>
2025-11-26 10:42:43 +0100

    hero.py fonctionnel + ajout fichier utilitairehero.py

---
+++ script/utilitaires/utilitairehero.py
@@ -0,0 +1,25 @@
+#Ensemble des variables globales
+
+
+
+
+
+#--------------------------------------------
+# Fonction de modification de competence de personnage en donnant le nom du personnage,
+# nom : str, signe : bollean, nbr : int
+def changercompetence(nom, signe, nbr):
+    print(nom,signe, nbr)
+    pass
+
+#--------------------------------------------
+# Fonction d'ouverture d'un menu
+def fctmenu():
+    pass
+#--------------------------------------------
+# Fonction d'ouverture d'inventaire
+def fctinventaire():
+    pass
+#--------------------------------------------
+# Fonction de visualisation de competence
+def fctcompetence():
+    pass
