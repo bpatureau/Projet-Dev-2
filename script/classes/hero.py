cdac145ffbb21519a37cb7cf3ab656571ce225d5
Caribouspatial <ju.aerts@students.ephec.be>
2025-11-27 12:30:40 +0100

    import de hero.py et utilitaires/utilitairehero.py dans __init__.py

--- script/classes/hero.py
+++ script/classes/hero.py
@@ -1,19 +1,20 @@
 #Ensemble des variables globales
 listeClasse = ["Inspecteur", "Commissaire", "Détective"]
+listeHero = {}
 
 # --------------------------------------------
 # Fonction de création de perso Hero
 def creationhero():
     nom = input("Entrez votre nom: ")
     classe = choixclassehero()
-    nouveauhero = Hero(nom, classe)
+    listeHero[nom] = Hero(nom, classe)
 
-    return nouveauhero
+    return listeHero[nom]
 
 #--------------------------------------------
 # Fonction de choix de classe de départ de l'instance de Hero
 def choixclassehero():
-    choix = input("Entrez votre choix parmi Inspecteur (a), Commissaire (b) et Détective (c): ")
+    choix = input("Entrez votre choix parmi \033[3mInspecteur\033[3m (a), \033[3mCommissaire\033[3m (b) et \033[3mDétective\033[3m (c): ")
     if choix == "a":
         return listeClasse[0]
     elif choix == "b":
@@ -46,13 +47,12 @@ class Hero:
         else: # Détective
             self.competence = {"force": 0, "charisme": 2, "intelligence": 2, "agilité": 0}
 
-        #self. inventaire = appel inventaire.py et lier à instance d'inventaire
+        # self.inventaire = appel inventaire.py et lier à instance d'inventaire
 
     def __str__(self):
-        return f"{self.nom} est un {self.classe} personage et à {self.competence['force']} de force"
+        return f"\033[3m{self.nom}\033[3m est un \033[3m{self.classe}\033[3m personage et à {self.competence['force']} de force"
 # --------------------------------------------
 #TEST
-
-michel = creationhero()
-print(michel)
+#michel = creationhero()
+#print(michel)
 #--------------------------------------------
--- script/utilitaires/utilitairehero.py
+++ script/utilitaires/utilitairehero.py
@@ -5,7 +5,7 @@
 
 
 #--------------------------------------------
-# Fonction de modification de competence de personnage en donnant le nom du personnage,
+# Fonction de modification de competence de personnage en donnant le nom du personnage
 # nom : str, signe : bollean, nbr : int
 def changercompetence(nom, signe, nbr):
     print(nom,signe, nbr)
