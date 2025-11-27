509c80dc9b67f8e806a7529f4fbf7d2f215a109e
bastien <bastien.patureau@yahoo.com>
2025-11-27 08:39:45 +0100

    Ajout dans la classe ListeDeChoix de la méthode _verifier _prerequis

--- script/classes/liste_de_choix.py
+++ script/classes/liste_de_choix.py
@@ -30,3 +30,37 @@ class ListeDeChoix:
         
         return disponibles
     
+    def _verifier_prerequis(self, prerequis, 
+                           hero, affinites):
+        """Vérifie si les prérequis d'un choix sont satisfaits"""
+        if prerequis is None:
+            return True
+        
+        # Vérifier objet requis
+        if "objet" in prerequis:
+            if not hero.inventaire.contient(prerequis["objet"]):
+                return False
+        
+        # Vérifier caractéristique minimale
+        if "caracteristique" in prerequis:
+            carac_nom = prerequis["caracteristique"]
+            carac_min = prerequis.get("valeur_min", 0)
+            if hero.caracteristiques.get(carac_nom, 0) < carac_min:
+                return False
+        
+        # Vérifier affinité avec PNJ
+        if "pnj" in prerequis:
+            pnj_nom = prerequis["pnj"]
+            affinite_min = prerequis.get("affinite_min", 0)
+            if affinites.get(pnj_nom, 0) < affinite_min:
+                return False
+        
+        # Vérifier or minimum
+        if "or_min" in prerequis:
+            if hero.inventaire.or_ < prerequis["or_min"]:
+                return False
+        
+        return True
+    
+
+
