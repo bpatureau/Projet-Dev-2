10f6d39af325956cf404d0372151d04b0b087654
bastien <bastien.patureau@yahoo.com>
2025-11-27 08:28:48 +0100

    Ajout dans la classe ListeDeChoix de la méthode ajouter_proposition

--- script/classes/liste_de_choix.py
+++ script/classes/liste_de_choix.py
@@ -5,3 +5,18 @@ class ListeDeChoix:
         self.id = id_node
         self.description = description
         self.propositions = []
+    
+    def ajouter_proposition(self, texte: str, consequence: str, 
+                           suivant = None, 
+                           prerequis = None,
+                           defaite = False):
+        """Ajoute une proposition de choix"""
+        proposition = {
+            "texte": texte,
+            "prerequis": prerequis,
+            "consequence": consequence,
+            "suivant": suivant,
+            "defaite": defaite
+        }
+        self.propositions.append(proposition)
+    
