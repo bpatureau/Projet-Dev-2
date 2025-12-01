399025d3c0ac85e028d332f137b5c401a2af2b0f
Caribouspatial <ju.aerts@students.ephec.be>
2025-12-01 11:26:01 +0100

    doc modifcompetance

--- script/utilitaires/utilitairehero.py
+++ script/utilitaires/utilitairehero.py
@@ -1,19 +1,23 @@
 #Ensemble des variables globales
 #--------------------------------------------
-# Fonction de modification de competence de personnage en donnant le nom du personnage
-# nom : str, signe : bollean, nbr : int
-def changercompetence(nom, signe, nbr):
+"""
+Fonction de modification de competence de personnage en donnant le nom du personnage
+nom : str # le nom de l'instance
+ signe : bollean # augmenter (+) ou diminuer (-) une competance
+  nbr : int # de combien augenter/diminuer
+"""
+def modifcompetence(nom, signe, nbr):
     print(nom,signe, nbr)
     pass
 #--------------------------------------------
 # Fonction d'ouverture d'un menu
-def fctmenu():
+def affmenu():
     pass
 #--------------------------------------------
 # Fonction d'ouverture d'inventaire
-def fctinventaire():
+def affinventaire():
     pass
 #--------------------------------------------
 # Fonction de visualisation de competence
-def fctcompetence():
+def affcompetence():
     pass
