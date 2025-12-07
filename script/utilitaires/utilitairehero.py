1b0a37163d30cb46ae24a61971383acb9e9caea1
Caribouspatial <ju.aerts@students.ephec.be>
2025-12-07 18:29:37 +0100

    transfert de la fonction de modification de compétance en méthode de classe dans hero.py

--- script/utilitaires/utilitairehero.py
+++ script/utilitaires/utilitairehero.py
@@ -1,23 +1,9 @@
 #Ensemble des variables globales
 #--------------------------------------------
-"""
-Fonction de modification de competence de personnage en donnant le nom du personnage
-nom : str # le nom de l'instance
- signe : bollean # augmenter (+) ou diminuer (-) une competance
-  nbr : int # de combien augenter/diminuer
-"""
-def modifcompetence(nom, signe, nbr):
-    print(nom,signe, nbr)
-    pass
-#--------------------------------------------
-# Fonction d'ouverture d'un menu
-def affmenu():
-    pass
-#--------------------------------------------
 # Fonction d'ouverture d'inventaire
 def affinventaire():
     pass
 #--------------------------------------------
-# Fonction de visualisation de competence
-def affcompetence():
+# Fonction d'ouverture d'un menu
+def affmenu():
     pass
