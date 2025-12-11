df4792d8bb07296f7d25c1b8f1d4017cafe87872
Lucario <yonas.eberhart@gmail.com>
2025-12-11 16:46:13 +0100

    3 working simple tests

--- script/test_unitaire/test_inventaire.py
+++ script/test_unitaire/test_inventaire.py
@@ -2,9 +2,27 @@ import pytest
 from script.classes.inventaire import Inventaire
 
 class TestInventaire:
-    def test_inventaire(self):
+    def test_creation_inventaire(self):
         inv = Inventaire()
         assert inv._argent == 0
+        assert len(inv.objets) == 0
+
+
+    def test_argent(self):
+        foo = Inventaire()
+        foo.argent = 12
+        assert foo.argent == 12
+        assert foo._argent == 12
+        foo._argent = 5
+        assert foo.argent == 5
+
+
+    def test_argent_cas_limites(self):
+        doo = Inventaire()
+        doo.argent = -100
+        assert doo.argent == -100
+        assert doo._argent == -100
+
         """assert inv.getObjets() == ""
 
         inv.addArgent(12)
