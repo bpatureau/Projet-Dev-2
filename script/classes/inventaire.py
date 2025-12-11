89445326c0e658e2aaa536ce7350aa9ef81d725a
Lucario <yonas.eberhart@gmail.com>
2025-12-11 15:01:15 +0100

    ajoute getter/setter pour argent et objets

--- script/classes/inventaire.py
+++ script/classes/inventaire.py
@@ -2,12 +2,28 @@ class Inventaire:
     def __init__(self):
         self.argent = 0
         self.objets = []
+
     def __str__(self):
         return (f"argent : {self.argent} \n"
                 f"objets : {self.objets}")
+
     @property
-    def getargent(self):
+    def getArgent(self):
         return self.argent
+
+    def getObjets(self):
+        out=""
+        for objet in self.objets:
+            out+=f"{objet}, "
+        return out
+
+    def addArgent(self, num):
+        self.argent+=num
+        return self.argent
+
+    def addObjets(self, obj):
+        self.objets.append(obj)
+        return self.objets
 #-----------------------------------
-#inventaireTest = Inventaire()
-#print(inventaireTest)
+inventaireTest = Inventaire()
+print(inventaireTest)
