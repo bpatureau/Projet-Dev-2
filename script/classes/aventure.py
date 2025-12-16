6e07a03427a071a86dfc11f1de5bbb1ee68c280f
arcade2885 <grolauxbaptiste@gmail.com>
2025-12-16 11:03:50 +0100

    modif pnj/ ajout affinitee. ajout de @property, Expression régulière, filtre/map, fonction lamda

--- script/classes/aventure.py
+++ script/classes/aventure.py
@@ -1,19 +1,51 @@
-from liste_de_choix import *
-from hero import *
+from .liste_de_choix import *
+from .hero import *
+import re
 
 class Aventure:
-    def __init__(self, nom:str, hero:Hero):
+    def __init__(self, nom:str, hero):
         self.nom = nom
         self.hero = hero
-        self.pnjs = {}
-        self.choixActuel=[]
+        self.pnj = {}
+        self.en_cours = False
     def __str__(self):
         return f"""Bienvenu dans l'aventure {self.nom},
- le héros {self.hero},
- avec les PNJ: {', '.join(self.pnjs.keys())},
- j'ai comme choix actuel: {self.choixActuel} """
-    def get_PNJ(self):
-        return self.pnjs
+ le héros {self.hero}"""
+    def ajouter_pnj(self, nom_pnj: str, affinite_initiale = 0):
+        if not re.match(r"^[A-Za-z]+$", nom_pnj):
+            print("Nom de PNJ invalide.")
+            return
+        self.pnj[nom_pnj] = max(0, min(100, affinite_initiale))
+    def switchAdventureState(self):
+        self.en_cours = not self.en_cours
+    def affinite_pnj(self, nom_pnj: str, variation: int):
+        if nom_pnj in self.pnj:
+            nouvelle_affinite = max(0, min(100, self.pnj[nom_pnj] + variation))
+            self.pnj[nom_pnj] = nouvelle_affinite
+    def afficher_affinites(self):
+        for nom_pnj, affinite in self.pnj.items():
+            print(f"{nom_pnj}: {affinite}")
+    def afficher_pnj_affinite(self, affinite_min: int):
+        pnj_amis = filter(lambda p: p[1] >= affinite_min, self.pnj.items())
+        noms = map(lambda p: p[0], pnj_amis)
+        print("PNJ amis :", list(noms))
+
+    @property
+    def get_pnj(self):
+        return self.pnj
+    @property
+    def get_hero(self):
+        return self.hero
+    @hero.setter
+    def hero(self, new_hero):
+        self.hero = new_hero
+    
+
+
+
+
+
+
     
 def creationAventure():
     nomAventure = input("Entrez le nom de votre aventure: ")
@@ -21,6 +53,3 @@ def creationAventure():
     aventure = Aventure(nomAventure, hero)
     return aventure
 
-aventureTest = creationAventure()   
-aventureTest.createChoixActuel()
-print(aventureTest)
